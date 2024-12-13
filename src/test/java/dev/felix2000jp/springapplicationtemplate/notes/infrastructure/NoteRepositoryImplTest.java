92327fad8d51a61963a497391afe09a00e138038
felix2000jp <felix2000jp@gmail.com>
2024-12-13 16:14:54 +0000

    fix repository tests

--- src/test/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/NoteRepositoryImplTest.java
+++ src/test/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/NoteRepositoryImplTest.java
@@ -1,16 +1,16 @@
 package dev.felix2000jp.springapplicationtemplate.notes.infrastructure;
 
 import dev.felix2000jp.springapplicationtemplate.notes.domain.Note;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -28,20 +28,29 @@ class NoteRepositoryImplTest {
 
     @Container
     @ServiceConnection
-    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres");
 
     @Autowired
-    private TestEntityManager entityManager;
+    private EntityManager entityManager;
     @Autowired
     private NoteRepositoryImpl noteRepository;
 
+    @BeforeEach
+    void setUp() {
+        entityManager
+                .createNativeQuery("TRUNCATE TABLE note")
+                .executeUpdate();
+    }
+
     @Test
-    @Transactional
     void should_find_notes_from_appuser_with_appuserId_when_page_is_not_empty() {
         var appuserId = UUID.randomUUID();
 
-        entityManager.persistAndFlush(new Note(appuserId, "title 1", "content 1"));
-        entityManager.persistAndFlush(new Note(appuserId, "title 2", "content 2"));
+        entityManager.persist(new Note(appuserId, "title 1", "content 1"));
+        entityManager.persist(new Note(appuserId, "title 2", "content 2"));
+
+        entityManager.flush();
+        entityManager.clear();
 
         var actual = noteRepository.getByAppuserId(appuserId, 0);
 
@@ -49,12 +58,14 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_not_find_notes_from_appuser_with_appuserId_when_page_is_empty() {
         var appuserId = UUID.randomUUID();
 
-        entityManager.persistAndFlush(new Note(appuserId, "title 1", "content 1"));
-        entityManager.persistAndFlush(new Note(appuserId, "title 2", "content 2"));
+        entityManager.persist(new Note(appuserId, "title 1", "content 1"));
+        entityManager.persist(new Note(appuserId, "title 2", "content 2"));
+
+        entityManager.flush();
+        entityManager.clear();
 
         var actual = noteRepository.getByAppuserId(appuserId, 1);
 
@@ -62,11 +73,13 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_find_note_with_id_from_appuser_with_appuserId_when_note_exists() {
         var note = new Note(UUID.randomUUID(), "title", "content");
 
-        entityManager.persistAndFlush(note);
+        entityManager.persist(note);
+
+        entityManager.flush();
+        entityManager.clear();
 
         var actual = noteRepository.getByIdAndAppuserId(note.getId(), note.getAppuserId());
 
@@ -74,7 +87,6 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_not_find_note_with_id_from_appuser_with_appuserId_when_note_does_not_exist() {
         var note = new Note(UUID.randomUUID(), "title", "content");
 
@@ -84,20 +96,21 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_delete_note_with_id_when_note_exists() {
         var note = new Note(UUID.randomUUID(), "title", "content");
-        entityManager.persistAndFlush(note);
+
+        entityManager.persist(note);
+
+        entityManager.flush();
+        entityManager.clear();
 
         noteRepository.deleteById(note.getId());
-        entityManager.flush();
 
         var actual = entityManager.find(Note.class, note.getId());
         assertNull(actual);
     }
 
     @Test
-    @Transactional
     void should_not_throw_when_trying_to_delete_note_with_id_when_note_does_not_exist() {
         assertDoesNotThrow(() -> {
             noteRepository.deleteById(UUID.randomUUID());
@@ -106,17 +119,18 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_delete_notes_from_appuser_with_id_when_notes_exist() {
         var appuserId = UUID.randomUUID();
         var note1 = new Note(appuserId, "title", "content");
         var note2 = new Note(appuserId, "title", "content");
 
-        entityManager.persistAndFlush(note1);
-        entityManager.persistAndFlush(note2);
+        entityManager.persist(note1);
+        entityManager.persist(note2);
+
+        entityManager.flush();
+        entityManager.clear();
 
         noteRepository.deleteByAppuserId(appuserId);
-        entityManager.flush();
 
         var actual1 = entityManager.find(Note.class, note1.getId());
         var actual2 = entityManager.find(Note.class, note2.getId());
@@ -125,7 +139,6 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_not_throw_when_trying_to_delete_notes_from_appuser_with_id_and_notes_do_not_exist() {
         assertDoesNotThrow(() -> {
             noteRepository.deleteByAppuserId(UUID.randomUUID());
@@ -134,12 +147,13 @@ class NoteRepositoryImplTest {
     }
 
     @Test
-    @Transactional
     void should_save_note_successfully() {
         var note = new Note(UUID.randomUUID(), "title 1", "content 1");
 
         noteRepository.save(note);
+
         entityManager.flush();
+        entityManager.clear();
 
         var noteFound = entityManager.find(Note.class, note.getId());
         assertNotNull(noteFound);
@@ -147,7 +161,6 @@ class NoteRepositoryImplTest {
 
     @ParameterizedTest
     @MethodSource
-    @Transactional
     void should_fail_to_save_note_when_note_data_is_invalid(Note note) {
         assertThrows(Exception.class, () -> {
             noteRepository.save(note);
