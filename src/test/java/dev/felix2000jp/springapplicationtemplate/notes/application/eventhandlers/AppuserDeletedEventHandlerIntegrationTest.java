2bc5f4010f57ee4dfcc6bd08e9b2fb2a2a4008b2
felix2000jp <felix2000jp@gmail.com>
2024-12-13 22:39:58 +0000

    add event handler integration tests

---
+++ src/test/java/dev/felix2000jp/springapplicationtemplate/notes/application/eventhandlers/AppuserDeletedEventHandlerIntegrationTest.java
@@ -0,0 +1,54 @@
+package dev.felix2000jp.springapplicationtemplate.notes.application.eventhandlers;
+
+import dev.felix2000jp.springapplicationtemplate.appusers.AppuserDeletedEvent;
+import dev.felix2000jp.springapplicationtemplate.notes.domain.Note;
+import dev.felix2000jp.springapplicationtemplate.notes.domain.NoteRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ApplicationModuleTest
+@Testcontainers
+class AppuserDeletedEventHandlerIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres");
+
+    @Autowired
+    private NoteRepository noteRepository;
+    @Autowired
+    private AppuserDeletedEventHandler eventHandler;
+
+    @Test
+    void should_delete_all_notes_from_appuser_when_event_with_appuserId_is_published(Scenario scenario) {
+        var appuserId = UUID.randomUUID();
+        var note1 = new Note(appuserId, "title 1", "content 1");
+        var note2 = new Note(appuserId, "title 2", "content 2");
+        var note3 = new Note(UUID.randomUUID(), "title 3", "content 3");
+
+        noteRepository.save(note1);
+        noteRepository.save(note2);
+        noteRepository.save(note3);
+
+        scenario
+                .publish(new AppuserDeletedEvent(appuserId))
+                .andWaitForStateChange(() -> eventHandler)
+                .andVerify(unusedEventHandler -> {
+                    assertNull(noteRepository.getByIdAndAppuserId(note1.getId(), note1.getAppuserId()));
+                    assertNull(noteRepository.getByIdAndAppuserId(note2.getId(), note2.getAppuserId()));
+                    assertNotNull(noteRepository.getByIdAndAppuserId(note3.getId(), note3.getAppuserId()));
+                });
+    }
+
+}
