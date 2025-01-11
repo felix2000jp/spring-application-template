ffe8c7b73f5a61365d9558e53af02cca971cd122
felix2000jp <felix2000jp@gmail.com>
2025-01-11 09:27:03 +0000

    remove shared module

--- src/main/java/dev/felix2000jp/springapplicationtemplate/shared/AuthenticatedUser.java
+++
@@ -1,7 +0,0 @@
-package dev.felix2000jp.springapplicationtemplate.shared;
-
-import java.util.Set;
-import java.util.UUID;
-
-public record AuthenticatedUser(UUID id, String username, Set<String> authorities) {
-}
--- src/main/java/dev/felix2000jp/springapplicationtemplate/shared/security/InvalidAuthenticationTypeException.java
+++
@@ -1,9 +0,0 @@
-package dev.felix2000jp.springapplicationtemplate.shared.security;
-
-class InvalidAuthenticationTypeException extends RuntimeException {
-
-    InvalidAuthenticationTypeException() {
-        super("Invalid authentication type, only Jwt is allowed");
-    }
-
-}
