6eebf7b2f3c1562464213608eed7ce5947d50a4e
felix2000jp <felix2000jp@gmail.com>
2025-01-12 17:23:22 +0000

    refactor note service impl test

--- src/test/java/dev/felix2000jp/springapplicationtemplate/notes/application/NoteServiceImplTest.java
+++ src/test/java/dev/felix2000jp/springapplicationtemplate/notes/application/NoteServiceImplTest.java
@@ -1,11 +1,11 @@
 package dev.felix2000jp.springapplicationtemplate.notes.application;
 
-import dev.felix2000jp.springapplicationtemplate.shared.SecurityService;
 import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.CreateNoteDto;
 import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.UpdateNoteDto;
 import dev.felix2000jp.springapplicationtemplate.notes.domain.Note;
 import dev.felix2000jp.springapplicationtemplate.notes.domain.NoteRepository;
 import dev.felix2000jp.springapplicationtemplate.notes.domain.exceptions.NoteNotFoundException;
+import dev.felix2000jp.springapplicationtemplate.shared.SecurityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,15 +43,16 @@ class NoteServiceImplTest {
                 "username",
                 Set.of(SecurityService.Scope.APPLICATION.name())
         );
+
+        when(securityService.getUser()).thenReturn(authenticatedUser);
     }
 
     @Test
-    void givenPage_whenGetByCurrent_User_thenReturnNotes() {
+    void givenPage_whenGetByCurrentUser_User_thenReturnNotes() {
         // given
         var page = 0;
         var note = new Note(UUID.randomUUID(), "title", "content");
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
         when(noteRepository.getByAppuserId(authenticatedUser.id(), 0)).thenReturn(List.of(note));
 
         // when
@@ -66,11 +67,10 @@ class NoteServiceImplTest {
     }
 
     @Test
-    void givenEmptyPage_whenGetByCurrent_User_thenReturnEmptyList() {
+    void givenEmptyPage_whenGetByCurrentUser_User_thenReturnEmptyList() {
         // given
         var page = 0;
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
         when(noteRepository.getByAppuserId(authenticatedUser.id(), 0)).thenReturn(List.of());
 
         // when
@@ -81,86 +81,102 @@ class NoteServiceImplTest {
     }
 
     @Test
-    void should_get_note_with_id_from_logged_in_appuser_when_note_exists() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
+    void givenId_getByIdAndCurrentUser_thenReturnNoteDto() {
+        // given
         var note = new Note(UUID.randomUUID(), "title", "content");
+        var id = note.getId();
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
-        when(noteRepository.getByIdAndAppuserId(note.getId(), authenticatedUser.id())).thenReturn(note);
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(note);
 
-        var actual = noteService.getByIdAndCurrentUser(note.getId());
+        // when
+        var actual = noteService.getByIdAndCurrentUser(id);
 
+        // then
         assertEquals(note.getId(), actual.id());
         assertEquals(note.getTitle(), actual.title());
         assertEquals(note.getContent(), actual.content());
     }
 
     @Test
-    void should_create_note_with_title_and_content_from_dto() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
-        var createNoteDTO = new CreateNoteDto("title", "content");
+    void givenNonExistentId_getByIdAndCurrentUser_thenThrowNoteNotFoundException() {
+        // given
+        var note = new Note(UUID.randomUUID(), "title", "content");
+        var id = note.getId();
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(null);
 
-        var actual = noteService.createByCurrentUser(createNoteDTO);
-
-        assertEquals("title", actual.title());
-        assertEquals("content", actual.content());
+        // when and then
+        assertThrows(NoteNotFoundException.class, () -> noteService.getByIdAndCurrentUser(id));
     }
 
     @Test
-    void should_update_note_with_title_and_content_from_dto() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
+    void givenCreateNoteDto_createByCurrentUser_thenCreateNote() {
+        // given
+        var createNoteDto = new CreateNoteDto("title", "content");
+
+        // when
+        var actual = noteService.createByCurrentUser(createNoteDto);
+
+        // then
+        assertEquals(createNoteDto.title(), actual.title());
+        assertEquals(createNoteDto.content(), actual.content());
+    }
+
+    @Test
+    void givenIdAndUpdateNoteDto_whenUpdateByIdAndCurrentUser_thenUpdateNote() {
+        // given
+        var note = new Note(UUID.randomUUID(), "title", "content");
+        var id = note.getId();
+        var updateNoteDto = new UpdateNoteDto("new title", "new content");
+
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(note);
+
+        // when
+        var actual = noteService.updateByIdAndCurrentUser(id, updateNoteDto);
+
+        // then
+        assertEquals(updateNoteDto.title(), actual.title());
+        assertEquals(updateNoteDto.content(), actual.content());
+    }
+
+    @Test
+    void givenNonExistentIdAndUpdateNoteDto_whenUpdateByIdAndCurrentUser_thenThrowNoteNotFoundException() {
+        // given
+        var id = UUID.randomUUID();
         var updateNoteDTO = new UpdateNoteDto("new title", "new content");
-        var note = new Note(UUID.randomUUID(), "title", "content");
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
-        when(noteRepository.getByIdAndAppuserId(note.getId(), authenticatedUser.id())).thenReturn(note);
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(null);
 
-        var actual = noteService.updateByIdAndCurrentUser(note.getId(), updateNoteDTO);
-
-        assertEquals("new title", actual.title());
-        assertEquals("new content", actual.content());
+        // when and then
+        assertThrows(NoteNotFoundException.class, () -> noteService.updateByIdAndCurrentUser(id, updateNoteDTO));
     }
 
     @Test
-    void should_throw_when_note_to_update_with_title_and_content_from_dto_is_not_found() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
-        var updateNoteDTO = new UpdateNoteDto("new title", "new content");
+    void givenId_whenDeleteByIdAndCurrentUser_thenDeleteNote() {
+        // given
         var note = new Note(UUID.randomUUID(), "title", "content");
+        var id = note.getId();
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
-        when(noteRepository.getByIdAndAppuserId(note.getId(), authenticatedUser.id())).thenReturn(null);
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(note);
 
-        var noteId = note.getId();
-        assertThrows(NoteNotFoundException.class, () -> noteService.updateByIdAndCurrentUser(noteId, updateNoteDTO));
-    }
+        // when
+        var actual = noteService.deleteByIdAndCurrentUser(id);
 
-    @Test
-    void should_delete_note_with_id_when_note_exists() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
-        var note = new Note(UUID.randomUUID(), "title", "content");
-
-        when(securityService.getUser()).thenReturn(authenticatedUser);
-        when(noteRepository.getByIdAndAppuserId(note.getId(), authenticatedUser.id())).thenReturn(note);
-
-        var actual = noteService.deleteByIdAndCurrentUser(note.getId());
-
+        // then
         assertEquals(note.getId(), actual.id());
         assertEquals(note.getTitle(), actual.title());
         assertEquals(note.getContent(), actual.content());
     }
 
     @Test
-    void should_throw_when_deleting_note_with_id_is_not_found() {
-        var authenticatedUser = new SecurityService.User(UUID.randomUUID(), "username", Set.of("Application"));
-        var note = new Note(UUID.randomUUID(), "title", "content");
+    void givenNonExistentId_whenDeleteByIdAndCurrentUser_thenThrowNoteNotFoundException() {
+        // given
+        var id = UUID.randomUUID();
 
-        when(securityService.getUser()).thenReturn(authenticatedUser);
-        when(noteRepository.getByIdAndAppuserId(note.getId(), authenticatedUser.id())).thenReturn(null);
+        when(noteRepository.getByIdAndAppuserId(id, authenticatedUser.id())).thenReturn(null);
 
-        var noteId = note.getId();
-        assertThrows(NoteNotFoundException.class, () -> noteService.deleteByIdAndCurrentUser(noteId));
+        // when and then
+        assertThrows(NoteNotFoundException.class, () -> noteService.deleteByIdAndCurrentUser(id));
     }
 
 }
