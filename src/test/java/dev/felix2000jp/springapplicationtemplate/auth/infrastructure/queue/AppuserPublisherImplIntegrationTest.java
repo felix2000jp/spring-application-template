b6bf96dba19a9334ab3ad1d5c641fc0137b2ae26
felix2000jp <felix2000jp@gmail.com>
2025-01-20 20:14:41 +0000

    refactor auth queue to infra

---
+++ src/test/java/dev/felix2000jp/springapplicationtemplate/auth/infrastructure/queue/AppuserPublisherImplIntegrationTest.java
@@ -0,0 +1,40 @@
+package dev.felix2000jp.springapplicationtemplate.auth.infrastructure.queue;
+
+import dev.felix2000jp.springapplicationtemplate.auth.application.AppuserPublisher;
+import dev.felix2000jp.springapplicationtemplate.auth.application.events.AppuserDeletedEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationModuleTest
+@Testcontainers
+class AppuserPublisherImplIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres");
+
+    @Autowired
+    private AppuserPublisher eventPublisher;
+
+    @Test
+    void publish_given_appuser_deleted_event_then_publish(Scenario scenario) {
+        var appuserId = UUID.randomUUID();
+        var appuserDeletedEvent = new AppuserDeletedEvent(appuserId);
+
+        scenario
+                .stimulate(() -> eventPublisher.publish(appuserDeletedEvent))
+                .andWaitForEventOfType(AppuserDeletedEvent.class)
+                .toArriveAndVerify(event -> assertThat(event.appuserId()).isEqualTo(appuserId));
+    }
+
+}
