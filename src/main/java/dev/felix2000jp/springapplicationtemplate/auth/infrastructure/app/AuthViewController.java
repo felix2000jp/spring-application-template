6c47795940e3c8c79026822616bc26d2f6a14c55
felix2000jp <felix2000jp@gmail.com>
2025-02-09 18:22:01 +0000

    setup session login

--- src/main/java/dev/felix2000jp/springapplicationtemplate/auth/infrastructure/app/AuthViewController.java
+++ src/main/java/dev/felix2000jp/springapplicationtemplate/auth/infrastructure/app/AuthViewController.java
@@ -1,13 +1,35 @@
 package dev.felix2000jp.springapplicationtemplate.auth.infrastructure.app;
 
+import dev.felix2000jp.springapplicationtemplate.auth.application.AuthService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Validated
 @Controller
+@RequestMapping("/app")
 class AuthViewController {
 
-    @GetMapping("/")
-    public String home() {
+    private final AuthService authService;
+
+    AuthViewController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping
+    public String registerPage() {
         return "index";
     }
 
