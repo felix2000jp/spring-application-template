8c388117a7cb3623b4dd401ba56670470e3e4176
felix2000jp <felix2000jp@gmail.com>
2025-02-11 20:44:51 +0000

    notes list

--- src/main/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/app/NoteViewController.java
+++ src/main/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/app/NoteViewController.java
@@ -1,12 +1,15 @@
 package dev.felix2000jp.springapplicationtemplate.notes.infrastructure.app;
 
 import dev.felix2000jp.springapplicationtemplate.notes.application.NoteService;
+import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.CreateNoteDto;
 import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.NoteListDto;
 import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,6 +28,13 @@ class NoteViewController {
     ResponseEntity<NoteListDto> getNotesForCurrentUser(@RequestParam(defaultValue = "0") @Min(0) int page) {
         var body = noteService.getNotesForCurrentUser(page);
         return ResponseEntity.ok(body);
+    }
+
+    @PostMapping
+    public String addNote(@RequestParam String title, @RequestParam String content, Model model) {
+        var newNote = noteService.createNoteForCurrentUser(new CreateNoteDto(title, content));
+        model.addAttribute("note", newNote);
+        return "note";
     }
 
 }
