6d38b4cb3c5a3d492bf9e95806b167b21f96bd24
felix2000jp <felix2000jp@gmail.com>
2025-02-22 11:27:21 +0000

    fix index page

--- src/main/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/app/NoteViewController.java
+++ src/main/java/dev/felix2000jp/springapplicationtemplate/notes/infrastructure/app/NoteViewController.java
@@ -2,20 +2,16 @@ package dev.felix2000jp.springapplicationtemplate.notes.infrastructure.app;
 
 import dev.felix2000jp.springapplicationtemplate.notes.application.NoteService;
 import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.CreateNoteDto;
-import dev.felix2000jp.springapplicationtemplate.notes.application.dtos.NoteListDto;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @Controller
-@RequestMapping("/app/notes")
+@RequestMapping("/app")
 class NoteViewController {
 
     private final NoteService noteService;
@@ -25,16 +21,19 @@ class NoteViewController {
     }
 
     @GetMapping
-    ResponseEntity<NoteListDto> getNotesForCurrentUser(@RequestParam(defaultValue = "0") @Min(0) int page) {
+    String getNotesForCurrentUser(@RequestParam(defaultValue = "0") @Min(0) int page, Model model) {
         var body = noteService.getNotesForCurrentUser(page);
-        return ResponseEntity.ok(body);
+        model.addAttribute("notes", body.notes());
+
+        return "index";
     }
 
-    @PostMapping
-    public String addNote(@RequestParam String title, @RequestParam String content, Model model) {
-        var newNote = noteService.createNoteForCurrentUser(new CreateNoteDto(title, content));
+    @PostMapping("/notes")
+    public String addNote(@Valid @ModelAttribute CreateNoteDto createNoteDto, Model model) {
+        var newNote = noteService.createNoteForCurrentUser(createNoteDto);
         model.addAttribute("note", newNote);
-        return "note";
+
+        return "notes/note-card";
     }
 
 }
